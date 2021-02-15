85ec64974a56b54f765699a92eb56652c46526b9
İbrahim Oğuzhan YILMAZ <ibrahimoguzhany@gmail.com>
2021-02-15 23:13:55 +0300

    Category&Product Controller Added

---
+++ Project.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.COMMON.Tools;
+using Project.ENTITIES.Models;
+using Project.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WebUI.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        ProductRepository _pRep;
+        CategoryRepository _cRep;
+
+        public ProductController()
+        {
+            _pRep = new ProductRepository();
+            _cRep = new CategoryRepository();
+        }
+        // GET: Admin/Product
+
+        public ActionResult ProductList(int? id)
+        {
+            ProductVM pvm = new ProductVM
+            {
+                Products = id == null ? _pRep.GetActives().ToList() : _pRep.Where(x => x.CategoryID == id).ToList()
+            };
+
+
+            return View(pvm);
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product, HttpPostedFileBase resim)
+        {
+            product.ImagePath = ImageUploader.UploadImage("~/Pictures/", resim);
+            _pRep.Add(product);
+
+            return RedirectToAction("ProductList");
+        }
+
+        public ActionResult UpdateProduct(int id)
+        {
+            ProductVM pvm = new ProductVM()
+            {
+                Categories = _cRep.GetActives(),
+                Product = _pRep.Find(id)
+            };
+
+            return View(pvm);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(Product product)
+        {
+            _pRep.Update(product);
+            return RedirectToAction("ProductList");
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            _pRep.Delete(_pRep.Find(id));
+            return RedirectToAction("ProductList");
+        }
+
+        
+    }
+}
